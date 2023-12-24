d69e7c05585a015ae9c3718e4b4b902a0dcc3c85
muzammilkhattri <khattrimuzammil@gmail.com>
2023-12-24 17:18:07 +0500

    WIP

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
         />
       </head>
-      <body className={`${significa.className} sm:px-40 px-5 w-full`}>
+      <body className={`${significa.className} lg:px-20 xl:px-40 px-5 w-full`}>
         <LogSnagProvider
           token="2aa19121e778b23407bc8d80765482ac"
           project="webifyr"
