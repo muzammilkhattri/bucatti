c60f8f4a824dee59a108cd6ef8ff96fe32651d95
muzammilkhattri <khattrimuzammil@gmail.com>
2024-01-22 14:40:22 +0500

    updated aboutus card

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -17,33 +17,36 @@ export default function About() {
         About us.
       </h1>
       <div className="flex flex-col md:flex-row justify-evenly space-x-4 mt-10">
-        <div className="bg-card p-8 rounded-md shadow-sm mt-4 w-[50%]">
-          <p className="text-2xl font-semibold  flex items-center">
+        <Card className="h-[300px]">
+          <CardHeader className="text-2xl font-bold items-center flex-row">
             <HelpCircle className="mr-2" /> Who we are
-          </p>
-          <div className="mt-4 text-gray-400">
-            <p>
+          </CardHeader>
+
+          <CardContent className="flex aspect-square items-start justify-center p-6">
+            <span className="text-xl">
               We are a team of young and passionate developers who are dedicated
               to providing the best services to our clients. We believe in the
               power of technology and we are always ready to help you bring your
               ideas to reality.
-            </p>
-          </div>
-        </div>
-        <div className="bg-card p-8 rounded-md shadow-sm mt-4 w-[50%]">
-          <p className="text-2xl font-semibold flex items-center">
+            </span>
+          </CardContent>
+        </Card>
+        <Card className="h-[300px]">
+          <CardHeader className="text-2xl font-bold items-center flex-row">
             <Rocket className="mr-2" /> Our Mission
-          </p>
-          <div className="mt-4 text-gray-400 ">
-            <p>
+          </CardHeader>
+
+          <CardContent className="flex aspect-square items-start justify-center p-6">
+            <span className="text-xl">
+              {" "}
               Our Mission At Webifyr is to empower businesses with cutting-edge
               web solutions that not only meet but exceed their expectations. We
               believe in the transformative power of well-executed web design
               and development to elevate brands, drive engagement, and deliver
               measurable results.
-            </p>
-          </div>
-        </div>
+            </span>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
